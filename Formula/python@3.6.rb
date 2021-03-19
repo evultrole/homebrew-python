87ab0044c9efd4532c7b44f95076b5328eafd7d1
Steven Hillis <steven@Stevens-MacBook-Pro.local>
2021-03-19 03:31:38 -0600

    patch

--- Formula/python@3.6.rb
+++ Formula/python@3.6.rb
@@ -41,7 +41,7 @@ class PythonAT36 < Formula
 
   patch do
     url "https://raw.githubusercontent.com/evultrole/aarch64-apple-gimp/main/fixes/python36.patch"
-    sha256 "620adeac59f2e6ace82d4ca9a9fbe617319801b4c123cf4af9d5a097ee6a3846"
+    sha256 "ca24d25aaad1f89bb5cd9c7cca5c56deb3968f9f0ac3f36c4d1de3c1348699e9"
   end
 
   skip_clean "bin/pip3", "bin/pip-3.4", "bin/pip-3.5", "bin/pip-3.6"
@@ -79,8 +79,6 @@ class PythonAT36 < Formula
       --enable-loadable-sqlite-extensions
       --without-ensurepip
       --with-dtrace
-      --host=aarch64-apple-darwin
-      --build=aarch64-apple-darwin
     ]
 
     args << "--without-gcc" if ENV.compiler == :clang
